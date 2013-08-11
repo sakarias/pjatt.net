31b2ddbfdf8d562c5104b7b52272a4d3299d580e
John Johannessen <johnj@slaskdot.org>
2013-08-11 21:11:23 +0200

    Changed the update script

--- _update.sh
+++ _update.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-jekyll
+case "$1" in
+  build)  jekyll build
+          ;;
 
-if [ $? -eq 0  ]
-then
-	rsync -avz -e ssh --delete _site/* delta.rockstable.net:/home/sites/pjatt.net/new_site/
+  deploy) rsync -az -e ssh --delete _site/* charlie.rockstable.net:/home/sites/pjatt.net/www/site/
+          open http://pjatt.net/
+          ;;
 
-	open http://pjatt.net
-fi
+  test)   rsync -az -e ssh --delete _site/* rs-web01.home.rockstable.net:/home/sites/pjatt/www/
+          open http://pjatt.home.rockstable.net/
+          ;;
 
+  *)      echo "Usage, $0 [build|deploy|test]"
+          ;;
+esac
